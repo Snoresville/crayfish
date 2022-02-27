01cc84fac8ee0666fe01518a28330ccfbaa58380
Snoresville <reflectiveflawless@hotmail.com>
2022-02-27 23:29:02 +1030

    i have no idea where i am at

--- content/panorama/scripts/custom_game/crayfish.js
+++ content/panorama/scripts/custom_game/crayfish.js
@@ -1,33 +1,165 @@
 // Game Settings
 const maxPots = 50;
+const crayfishPrice = 10;
+const crayfishGains = [
+    [1, 2],
+    [2, 5],
+    ["lose", "lose"],
+    [0, 2],
+    [2, "lose"],
+    [1, 1],
+];
 
 // Current Game
 let pots = 10;
 let money = 0;
+let currentDay = 1;
 
-// Mechanics
-const diceImage = $("#CrayfishDice");
+// Update
+const shallowPots = $("#ShallowPots");
+const deepPots = $("#DeepPots");
+function OnCrayfishPotsPlaced(location) {
+    const panelLocation = $(`#${location}`);
 
-function DiceRollValue() {
-    return Math.floor(Math.random() * 6) + 1;
+    if (isNaN(parseInt(panelLocation.text))) {
+        panelLocation.text = 0;
+    } else {
+        panelLocation.text = parseInt(panelLocation.text);
+    }
+
+    UpdateRollStatus();
 }
 
+const resourceMoney = $("#CrayfishResourcesMoney");
+const resourcePots = $("#CrayfishResourcesPots");
+function UpdateResourceCounts() {
+    resourceMoney.text = `Money: $${money}`;
+    resourcePots.text = `Pots: ${pots}`;
+}
+
+const rollStatusBar = $("#CrayfishRollChecker");
+const rollStatusText = $("#CrayfishRollCheckerStatus");
+let rollStatus = false;
+function UpdateRollStatus() {
+    rollStatus = true;
+    // $.Msg(shallowPots.text);
+    // $.Msg(deepPots.text);
+
+    const totalPotsPlanned =
+        parseInt(shallowPots.text) + parseInt(deepPots.text);
+
+    if (isNaN(totalPotsPlanned)) {
+        rollStatus = false;
+        rollStatusText.text = "Need inputs for both regions!";
+    }
+
+    if (totalPotsPlanned > pots) {
+        rollStatus = false;
+        rollStatusText.text = "Placed pots exceeds pot count!";
+    }
+
+    if (rollStatus) {
+        rollStatusText.text = "All Good!";
+    }
+    rollStatusBar.SetHasClass("Bad", !rollStatus);
+}
+
+function FullUpdate() {
+    UpdateRollStatus();
+    UpdateResourceCounts();
+}
+
+// Mechanics
+let currentlyRolling = false;
 function RollDice() {
+    if (!rollStatus || currentlyRolling) return;
+    currentlyRolling = true;
+
+    UpdateRollStatus();
+
     Game.EmitSound("dice");
 
     const result = DiceRollValue();
-
     for (let i = 0; i <= 5; i++) {
         $.Schedule(i * 0.1, () => SetDiceImage(DiceRollValue()));
     }
 
-    $.Schedule(1, () => SetDiceImage(result));
+    $.Schedule(1, () => {
+        SetDiceImage(result);
+        RollResult(result);
+        currentDay++;
+    });
+}
+
+const history = $("#CrayfishHistory");
+function RollResult(value) {
+    const rollValues = crayfishGains[value - 1];
+
+    const shallowPotCount = parseInt(shallowPots.text);
+    const deepPotCount = parseInt(deepPots.text);
+
+    let lostPots = 0;
+    let crayfish = 0;
+
+    if (rollValues[0] == "lose") {
+        lostPots += shallowPotCount;
+    } else {
+        crayfish += rollValues[0] * shallowPotCount;
+    }
+
+    if (rollValues[1] == "lose") {
+        lostPots += deepPotCount;
+    } else {
+        crayfish += rollValues[1] * deepPotCount;
+    }
+
+    shallowPots.text = "";
+    deepPots.text = "";
+
+    money += crayfish * crayfishPrice;
+    pots -= lostPots;
+    currentlyRolling = false;
+
+    FullUpdate();
+
+    WriteHistoryLine(`---[ Day ${currentDay} ]---`);
+    if (lostPots > 0) WriteHistoryLine(`Lost ${lostPots} pots...`);
+    WriteHistoryLine(
+        `Got ${crayfish} Crayfish! (+$${crayfish * crayfishPrice})`
+    );
+}
+
+function OnPotsPlaced(location) {
+    const locationPanel = $(`#${location}`);
+
+    $.Msg(locationPanel);
+
+    if (isNaN(parseInt(locationPanel.text))) {
+        locationPanel.text = 0;
+    } else {
+        locationPanel.text = parseInt(locationPanel.val);
+    }
+}
+
+// Misc
+function DiceRollValue() {
+    return Math.floor(Math.random() * 6) + 1;
 }
 
 let lastDice = 0;
+const diceImage = $("#CrayfishDice");
 function SetDiceImage(roll) {
     diceImage.SetHasClass(`Roll${lastDice}`, false);
     diceImage.SetHasClass(`Roll${roll}`, true);
 
     lastDice = roll;
 }
+
+function WriteHistoryLine(newString) {
+    const newLine = $.CreatePanel("Label", history, ``);
+    newLine.text = newString;
+}
+
+// Start Updating
+UpdateResourceCounts();
+UpdateRollStatus();
